c8c2b97d011a833408af5eb7d2ef9c97b35ff990
tony.astolfi <tony.astolfi@tamr.com>
2017-01-04 07:37:58 -0500

    More test coverage for parse_error

--- hittop/parser/parse_error-test.cc
+++ hittop/parser/parse_error-test.cc
@@ -1,14 +1,16 @@
-#include "hittop/parser/parser.h"
+#include "hittop/parser/parse_error.h"
+#include "hittop/parser/parse_error.h"
 
 #include <memory>
 #include <utility>
 
 #include "gtest/gtest.h"
 
-using hittop::parser::Fallible;
 using hittop::parser::ParseError;
 
 TEST(ParserErrorTest, Ctor) {
   std::error_condition c{ParseError::BAD_CHAR};
   EXPECT_EQ(c.value(), static_cast<int>(ParseError::BAD_CHAR));
+  EXPECT_EQ(c, ParseError::BAD_CHAR);
+  EXPECT_NE(c, ParseError::INCOMPLETE);
 }
