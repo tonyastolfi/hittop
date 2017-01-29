a4346c910472286380251dddaf5bc13fd6d4138e
tony.astolfi <tony.astolfi@tamr.com>
2017-01-29 15:32:13 -0500

    Fill in more of the HTTP/1.1 grammar.

--- hittop/http/grammar.h
+++ hittop/http/grammar.h
@@ -3,6 +3,7 @@
 
 #include "hittop/parser/any_char.h"
 #include "hittop/parser/at_least.h"
+#include "hittop/parser/char_filter.h"
 #include "hittop/parser/concat.h"
 #include "hittop/parser/either.h"
 #include "hittop/parser/implied_delim.h"
@@ -17,7 +18,10 @@ namespace grammar {
 
 using OCTET = parser::AnyChar;
 
+using DIGIT = parser::CharFilter<&std::isdigit>;
+
 using CRLF = parser::Concat<parser::Literal<'\r'>, parser::Literal<'\n'>>;
+
 using LWS = parser::Concat<parser::Opt<CRLF>,
                            parser::AtLeast<1, parser::Either<' ', '\t'>>>;
 
@@ -34,19 +38,56 @@ using separators = parser::Either<
     parser::Literal<'{'>, parser::Literal<'}'>, parser::Literal<' '>,
     parser::Literal<'\t'>>;
 
-using token = parser::AtLeast<1, Unless<Either<CTLs, separators>, AnyChar>>;
+using CTLs = parser::CharFilter<&std::iscntrl>;
 
-using field_content = ;
+using TEXT = parser::Either<LWS, parser::Unless<CTLs, parser::AnyChar>>;
 
-using field_value = Repeat<Either<field_content, LWS>>;
+using quoted_pair = parser::Concat<parser::Literal<'\\'>, parser::AnyChar>;
+
+using ctext =
+    parser::Unless<parser::Either<parser::Literal<'('>, parser::Literal<')'>>,
+                   TEXT>;
+
+using comment =
+    parser::Concat<parser::Literal<'('>,
+                   parser::Repeat<parser::Either<ctext, quoted_pair, comment>>,
+                   parser::Literal<')'>>;
+
+using qdtext = parser::Unless<parser::Literal<'"'>, parser::AnyChar>;
+
+using quoted_string =
+    parser::Concat<parser::Literal<'"'>,
+                   parser::Repeat<parser::Either<qdtext, quoted_pair>>,
+                   parser::Literal<'"'>>;
+
+using token = parser::AtLeast<
+    1, parser::Unless<parser::Either<CTLs, separators>, parser::AnyChar>>;
+
+/* The definition of field-content (RFC/2616, section 4.2, page 32) strikes me
+ * as particularly vague:
+ *
+ *   field-content  = <the OCTETs making up the field-value
+ *                    and consisting of either *TEXT or combinations
+ *                    of token, separators, and quoted-string>
+ *
+ * This is my best attempt at a translation to something actually implementable.
+ * :-/
+ */
+using field_content = parser::Either<
+    parser::Repeat<TEXT>,
+    parser::Repeat<parser::Either<token, separators, quoted_string>>>;
+
+using field_value = parser::Repeat<parser::Either<field_content, LWS>>;
 
 using field_name = token;
 
-using message_header = Glue<field_name, parser::Literal<':'>, Opt<field_value>>;
+using message_header =
+    Glue<field_name, parser::Literal<':'>, parser::Opt<field_value>>;
 
-namespace tokens {
+namespace token {
 
 DEFINE_NAMED_TOKEN(Cache_Control, "Cache-Control");
+DEFINE_NAMED_TOKEN(Connection, "Connection");
 DEFINE_NAMED_TOKEN(Content_Encoding, "Content-Encoding");
 DEFINE_NAMED_TOKEN(Content_Language, "Content-Language");
 DEFINE_NAMED_TOKEN(Content_Length, "Content-Length");
@@ -54,87 +95,98 @@ DEFINE_NAMED_TOKEN(Content_Location, "Content-Location");
 DEFINE_NAMED_TOKEN(Content_MD5, "Content-MD5");
 DEFINE_NAMED_TOKEN(Content_Range, "Content-Range");
 DEFINE_NAMED_TOKEN(Content_Type, "Content-Type");
-DEFINE_TOKEN(Connection);
-DEFINE_TOKEN(Date);
-DEFINE_TOKEN(Pragma);
-DEFINE_TOKEN(Trailer);
+DEFINE_NAMED_TOKEN(Date, "Date");
+DEFINE_NAMED_TOKEN(Pragma, "Pragma");
+DEFINE_NAMED_TOKEN(Trailer, "Trailer");
 DEFINE_NAMED_TOKEN(Transfer_Encoding, "Transfer-Encoding");
-DEFINE_TOKEN(Upgrade);
-DEFINE_TOKEN(Via);
-DEFINE_TOKEN(Warning);
+DEFINE_NAMED_TOKEN(Upgrade, "Upgrade");
+DEFINE_NAMED_TOKEN(Via, "Via");
+DEFINE_NAMED_TOKEN(Warning, "Warning");
 
 } // namespace tokens
 
-using entity_body = Repeat<OCTET>;
+using entity_body = parser::Repeat<OCTET>;
 
 using message_header = Glue<field_name, parser::Literal<':'>, Opt<field_value>>;
 
-using Content_Encoding = Glue<tokens::Content_Encoding, parser::Literal<':'>, >;
+using Content_Encoding =
+    Glue<tokens::Content_Encoding, parser::Literal<':'>, field_value>;
+
+using Content_Language =
+    Glue<tokens::Content_Language, parser::Literal<':'>, field_value>;
+
+using Content_Length = Glue<tokens::Content_Length, parser::Literal<':'>,
+                            parser::AtLeast<1, DIGIT>>;
+
+using Content_Location = Glue<tokens::Content_Location, parser::Literal<':'>,
+                              parser::Either<absoluteURI, relativeURI>>;
 
 using extension_header = message_header;
 
-using entity_header = Either<Allow,            //
-                             Content_Encoding, //
-                             Content_Language, //
-                             Content_Length,   //
-                             Content_Location, //
-                             Content_MD5,      //
-                             Content_Range,    //
-                             Content_Type,     //
-                             Expires,          //
-                             Last_Modified,    //
-                             extension_header>;
+using entity_header = Parser::Either<Allow,            //
+                                     Content_Encoding, //
+                                     Content_Language, //
+                                     Content_Length,   //
+                                     Content_Location, //
+                                     Content_MD5,      //
+                                     Content_Range,    //
+                                     Content_Type,     //
+                                     Expires,          //
+                                     Last_Modified,    //
+                                     extension_header>;
 
-using response_header = Either<Accept_Ranges,      //
-                               Age,                //
-                               ETag,               //
-                               Location,           //
-                               Proxy_Authenticate, //
-                               Retry_After,        //
-                               Server,             //
-                               Vary,               //
-                               WWW_Authenticate>;
+using response_header = Parser::Either<Accept_Ranges,      //
+                                       Age,                //
+                                       ETag,               //
+                                       Location,           //
+                                       Proxy_Authenticate, //
+                                       Retry_After,        //
+                                       Server,             //
+                                       Vary,               //
+                                       WWW_Authenticate>;
 
-using Reason_Phrase = Repeat<Unless<Either<parser::Literal<'\r'>, //
-                                           parser::Literal<'\n'>>,
-                                    TEXT>>;
+using Reason_Phrase =
+    parser::Repeat<paser::Unless<parser::Either<parser::Literal<'\r'>, //
+                                                parser::Literal<'\n'>>,
+                                 TEXT>>;
 
-using extension_code = Exactly<3, Digit>;
+using extension_code = parser::Exactly<3, DIGIT>;
 
-using Status_Code = Either< //
+using Status_Code = parser::Either< //
     // TODO(tonyastolfi) - put in all the actual status-codes here?
     extension_code>;
 
 using Status_Line =
-    Concat<HTTP_Version, SP, Status_Code, SP, Reason_Phrase, CRLF>;
+    parser::Concat<HTTP_Version, SP, Status_Code, SP, Reason_Phrase, CRLF>;
 
-using Response = Concat<Status_Line,                          //
-                        Repeat<Concat<Either<general_header,  //
-                                             response_header, //
-                                             entity_header>,
-                                      CRLF>>, //
-                        CRLF,                 //
-                        Opt<message_body>>;
+using Response = parser::Concat<
+    Status_Line,                                                  //
+    parser::Repeat<parser::Concat<parser::Either<general_header,  //
+                                                 response_header, //
+                                                 entity_header>,
+                                  CRLF>>, //
+    CRLF,                                 //
+    parser::Opt<message_body>>;
 
-using request_header = Either<Accept,              //
-                              Accept_Charset,      //
-                              Accept_Encoding,     //
-                              Accept_Language,     //
-                              Authorization,       //
-                              Expect,              //
-                              From,                //
-                              Host,                //
-                              If_Match,            //
-                              If_Modified_Since,   //
-                              If_None_Match,       //
-                              If_Range,            //
-                              If_Unmodified_Since, //
-                              Max_Forwards,        //
-                              Proxy_Authorization, //
-                              Range,               //
-                              Referer,             //
-                              TE,                  //
-                              User_Agent>;
+using request_header = parser::Either<Accept,              //
+                                      Accept_Charset,      //
+                                      Accept_Encoding,     //
+                                      Accept_Language,     //
+                                      Authorization,       //
+                                      Expect,              //
+                                      From,                //
+                                      Host,                //
+                                      If_Match,            //
+                                      If_Modified_Since,   //
+                                      If_None_Match,       //
+                                      If_Range,            //
+                                      If_Unmodified_Since, //
+                                      Max_Forwards,        //
+                                      Proxy_Authorization, //
+                                      Range,               //
+                                      Referer,             //
+                                      TE,                  //
+                                      User_Agent>;
 
 using extension_method = token;
 
@@ -147,48 +199,51 @@ DEFINE_TOKEN(DELETE);
 DEFINE_TOKEN(TRACE);
 DEFINE_TOKEN(CONNECT);
 
-using Method = Either<OPTIONS, //
-                      GET,     //
-                      HEAD,    //
-                      POST,    //
-                      PUT,     //
-                      DELETE,  //
-                      TRACE,   //
-                      CONNECT, //
-                      extension_method>;
+using Method = parser::Either<OPTIONS, //
+                              GET,     //
+                              HEAD,    //
+                              POST,    //
+                              PUT,     //
+                              DELETE,  //
+                              TRACE,   //
+                              CONNECT, //
+                              extension_method>;
 
-using general_header = Either<Cache_Control,     //
-                              Connection,        //
-                              Date,              //
-                              Pragma,            //
-                              Trailer,           //
-                              Transfer_Encoding, //
-                              Upgrade,           //
-                              Via,               //
-                              Warning>;
+using general_header = parser::Either<Cache_Control,     //
+                                      Connection,        //
+                                      Date,              //
+                                      Pragma,            //
+                                      Trailer,           //
+                                      Transfer_Encoding, //
+                                      Upgrade,           //
+                                      Via,               //
+                                      Warning>;
 
 using Request_URI =
-    Either<parser::Literal<'*'>, absoluteURI, abs_path, authority>;
+    parser::Either<parser::Literal<'*'>, absoluteURI, abs_path, authority>;
 
 using Request_Line = Concat<Method, SP, Request_URI, SP, HTTP_Version, CRLF>;
 
-using Request = Concat<Request_Line,                        //
-                       Repeat<Concat<Either<general_header, //
-                                            request_header, //
-                                            entity_header>, //
-                                     CRLF>>,                //
-                       CRLF,                                //
-                       Opt<message_body>>;
+using Request = parser::Concat<
+    Request_Line,                                                //
+    parser::Repeat<parser::Concat<parser::Either<general_header, //
+                                                 request_header, //
+                                                 entity_header>, //
+                                  CRLF>>,                        //
+    CRLF,                                                        //
+    parser::Opt<message_body>>;
 
-using message_body = Success; // This part of the grammar isn't context free
+using message_body =
+    parser::Success; // This part of the grammar isn't context free
 
-using start_line = Either<Request_Line, Status_Line>;
+using start_line = parser::Either<Request_Line, Status_Line>;
 
-using generic_message = Concat<start_line,                         //
-                               Repeat<Glue<message_header, CRLF>>, //
-                               CRLF, Opt<message_body>>;
+using generic_message =
+    parser::Concat<start_line,                                 //
+                   parser::Repeat<Glue<message_header, CRLF>>, //
+                   CRLF, parser::Opt<message_body>>;
 
-using HTTP_message = Either<Request, Response>;
+using HTTP_message = parser::Either<Request, Response>;
 
 } // namespace grammar
 } // namespace http
