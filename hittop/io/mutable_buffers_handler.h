f6658d71029283e6ca41eb438ff224de387eec15
tony.astolfi <tony.astolfi@tamr.com>
2017-03-06 17:29:01 -0500

    PR feedback.

--- hittop/io/mutable_buffers_handler.h
+++ hittop/io/mutable_buffers_handler.h
@@ -2,30 +2,20 @@
 #define MUTABLE_BUFFERS_HANDLER_H
 
 #include <functional>
-#include <vector>
+
+#include "boost/container/static_vector.hpp"
 
 #include "hittop/io/types.h"
 
 namespace hittop {
 namespace io {
 
-// AsyncMutableBufferStream Concept
-//
-// Requirements:
-//
-//  void async_prepare(std::size_t minimum_size, MutableBuffersHandler);
-//  std::size_t max_space() const;
-//  std::size_t space() const;
-//  void commit(std::size_t byte_count);
-//
-
 // MutableBufferHandler Concept
 //
 using MutableBuffersHandler = std::function<void(
-    const error_code &, cosnt std::vector<mutable_buffer> &)>;
+    const error_code &, cosnt boost::static_vector<mutable_buffer, 4> &)>;
 
 } // namespace io
 } // namespace hitttop
-
 
 #endif // MUTABLE_BUFFERS_HANDLER_H
